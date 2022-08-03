b830542e1c1890c76ffbbe0beddbec53574d2feb
aryar <arya.rizky4@gmail.com>
2022-08-03 10:16:15 +0700

    made the app.js leaner

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+
+function Expenses(props) {
+    return (
+        <div className='expenses'>
+            <h2>Let's get started!</h2>
+            <ExpenseItem title={props.expenseItem[0].title} amount={props.expenseItem[0].amount} date={props.expenseItem[0].date}></ExpenseItem>
+            <ExpenseItem title={props.expenseItem[1].title} amount={props.expenseItem[1].amount} date={props.expenseItem[1].date}></ExpenseItem>
+            <ExpenseItem title={props.expenseItem[2].title} amount={props.expenseItem[2].amount} date={props.expenseItem[2].date}></ExpenseItem>
+            <ExpenseItem title={props.expenseItem[3].title} amount={props.expenseItem[3].amount} date={props.expenseItem[3].date}></ExpenseItem>
+        </div>
+        
+    )
+}
+
+export default Expenses
